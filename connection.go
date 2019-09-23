3f839d3d409e66b9c2157cd607fd3b987d85b6b6
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2019-09-23 16:00:44 +0200

    Initial commit

---
+++ connection.go
@@ -0,0 +1,120 @@
+package rabbitmq
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func SetLogger(log Logger, level LoggingLevel) func(*Connection) {
+	return func(connection *Connection) {
+		connection.logger = logger{
+			Logger: log,
+			level:  level,
+		}
+	}
+}
+
+type Connection struct {
+	conn  *amqp.Connection
+	connM sync.RWMutex
+
+	logger          logger
+	notifyConnClose chan *amqp.Error
+	reconnectDelay  time.Duration
+
+	done chan bool
+
+	publishers []publisher
+}
+
+func Connect(dsn string, opts ...func(*Connection)) (*Connection, error) {
+	return ConnectCtx(context.Background(), dsn, opts...)
+}
+
+func ConnectCtx(ctx context.Context, dsn string, opts ...func(*Connection)) (*Connection, error) {
+	conn := Connection{
+		logger:         logger{Logger: DeafLogger()},
+		done:           make(chan bool),
+		reconnectDelay: 5 * time.Second,
+	}
+
+	for _, opt := range opts {
+		opt(&conn)
+	}
+
+	if err := conn.handleReconnect(ctx, dsn); err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			select {
+			case err, ok := <-conn.notifyConnClose:
+				if !ok {
+					// graceful shutdown
+					return
+				}
+				conn.logger.Debugf("connection closed: %v", err)
+				if err := conn.handleReconnect(context.Background(), dsn); err != nil {
+					conn.logger.Debugf("reconnect error: %v", err)
+				}
+			case <-conn.done:
+				return
+			}
+		}
+	}()
+
+	return &conn, nil
+}
+
+func (c *Connection) handleReconnect(ctx context.Context, dsn string) error {
+	for {
+		conn, err := amqp.Dial(dsn)
+		if err != nil {
+			c.logger.Debugf("failed to dial connection: %v", err)
+
+			select {
+			case <-c.done:
+				return fmt.Errorf("connection is closed")
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(c.reconnectDelay):
+				c.logger.Debug("connection reconnect")
+				continue
+			}
+		}
+
+		c.changeConnection(conn)
+		return nil
+	}
+}
+
+func (c *Connection) connection() *amqp.Connection {
+	c.connM.RLock()
+	defer c.connM.RUnlock()
+	return c.conn
+}
+
+func (c *Connection) changeConnection(connection *amqp.Connection) {
+	c.connM.Lock()
+	defer c.connM.Unlock()
+
+	c.conn = connection
+	c.notifyConnClose = make(chan *amqp.Error)
+	c.conn.NotifyClose(c.notifyConnClose)
+}
+
+func (c *Connection) Close() error {
+	close(c.done)
+
+	c.connM.Lock()
+	defer c.connM.Unlock()
+
+	if c.conn == nil {
+		return nil
+	}
+	return c.conn.Close()
+}
